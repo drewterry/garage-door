7762dacb829d2ecbe43ee8b107f97128229bdb92
Drew Terry <nick.terry@gmail.com>
2018-06-16 15:30:21 -0400

    add restify and gpio

---
+++ index.js
@@ -0,0 +1,15 @@
+import rpio from 'rpio';
+import { createServer } from 'restify';
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(80, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
